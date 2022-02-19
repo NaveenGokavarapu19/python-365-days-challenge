260b6b7e15a4383a6d9db2759df1347e7a59fe7a
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-19 21:58:45 +0530

    Added program to display hex,octal and square root of a number

---
+++ 19-02-2022/problem-2.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value of num: "))
+amt = float(input("Enter the value of amt: "))
+pi = float(input("Enter the value of pi: "))
+code = str(input("Enter the value of code: "))
+population_of_India = int(input("Enter the value of population of India: "))
+msg = str(input("Ener the value of message: "))
+#Print the values of variables 
+print("NUM =" + str(num) + "\n AMT = " + str(amt) + "\n CODE = " + str(code) + "\n" )
+print("POPULATION_OF_INDIA: " + str(population_of_India) + "\n MESSAGE = " + str(msg))
