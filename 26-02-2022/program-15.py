ce14b2f6f28f7593479594bd15f462461b1ff110
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-26 16:25:21 +0530

    Program to convert a floating point number to corresponding integer

---
+++ 26-02-2022/program-15.py
@@ -0,0 +1,6 @@
+'''
+program to convert a floating point number to corresponding integer
+'''
+
+a = float(input("Enter any floating point number = "))
+print("The integer variant of " + str(a) + " = " + str(int(a)))
