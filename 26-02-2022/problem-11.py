ea2d45471cde55d8664cfe37c3bb6fd1d275d518
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-26 13:44:05 +0530

    Program to convert degrees fahrenheit to celsius

---
+++ 26-02-2022/problem-11.py
@@ -0,0 +1,8 @@
+'''
+program to convert fahrenheit into degrees
+
+'''
+
+fahrenheit = float(input("Enter the temperature in fahrenheit: "))
+celsius = (0.56) * (fahrenheit-32)
+print("Temperature in degrees celsius = ",celsius)
