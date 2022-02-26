15b4b4548b1493726369086fa3251d7c868a3a7b
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-26 13:31:44 +0530

    Program to print the digit at one's place

---
+++ 26-02-2022/problem-9.py
@@ -0,0 +1,6 @@
+'''
+Program to print the digit at one's place of a number
+'''
+num = int(input("Enter any number: "))
+digit_at_ones_place = num%10
+print("The digit at ones place pf " + str(num) + " is " + str(digit_at_ones_place))
