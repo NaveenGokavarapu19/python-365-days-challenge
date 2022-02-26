a9cb9f7bfc7933e56a720526e9c990f3facbd2c1
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-26 13:23:37 +0530

    Program to demonstrate the use of relational operators

---
+++ 26-02-2022/problem-7.py
@@ -0,0 +1,12 @@
+'''
+Program to demonstrate the use of relational operators
+'''
+
+x = 10
+y = 20
+
+print(str(x) + " < " + str(y) + " = " + str(x<y))
+print(str(x) + " == " + str(y) + " = " + str(x==y))
+print(str(x) + " != " + str(y) + " = " + str(x!=y))
+print(str(x) + " > " + str(y) + " = " + str(x>y))
+print(str(x) + " >= " + str(y) + " = " + str(x<=y))
