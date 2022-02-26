09bbac915ffc32799ef5d9bf7d5f825efd467aa3
Naveen <gokavarapu.naveen97@gmail.com>
2022-02-26 12:47:09 +0530

    Program to calculate the distance between two points

---
+++ 26-02-2022/problem-4.py
@@ -0,0 +1,11 @@
+'''
+Program to calculate the distance between two points chapter 3
+'''
+
+x1 = int(input("Enter the x coordinate of the first point: "))
+y1 = int(input("Enrer the y coordinate of the first point: "))
+x2 = int(input("Enter the x coordinte of the second point: "))
+y2 = int(input("Enter the y coordinate of the second point: "))
+distance  = ((x2-x1) ** 2 + (y2-y1)**2)**0.5
+print("Distance = ")
+print(distance)
