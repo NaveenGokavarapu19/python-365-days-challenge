1245713e0dcf111816231e05c63cc2cc88d04294
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 13:54:03 +0530

    Program to demonstrate all arithmetic operations  on two integers on float numbers

---
+++ 01-03-2022/ex2.py
@@ -0,0 +1,22 @@
+'''
+Program to demonstrate all arithmetic operations  on two integers on float numbers
+'''
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+add_res = num1 + num2
+sub_res = num1 - num2
+mul_res = num1 * num2
+div_res = num1 / num2
+floor_res = num1 // num2
+mod_res = num1 % num2
+exp_res = num1 ** num2
+
+print("num1 + num2 = " + add_res)
+print("num1 - num2 = " + sub_res)
+print("num1 * num2 = " + mul_res)
+print("num1 / num2 = " + div_res)
+print("num1 // num2 = " + floor_res)
+print("num1 % num2 = " + mod_res)
+print("num1 ** num2 = " + exp_res)
