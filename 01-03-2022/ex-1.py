56c6c5ad8a15fd14df8883c9c765577425b4dbcc
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 15:28:12 +0530

    'Typecasted result to str to solve concatination error'

--- 01-03-2022/ex-1.py
+++ 01-03-2022/ex-1.py
@@ -13,10 +13,10 @@ floor_res = num1 // num2
 mod_res = num1 % num2
 exp_res = num1 ** num2
 
-print("num1 + num2 = " + add_res)
-print("num1 - num2 = " + sub_res)
-print("num1 * num2 = " + mul_res)
-print("num1 / num2 = " + div_res)
-print("num1 // num2 = " + floor_res)
-print("num1 % num2 = " + mod_res)
-print("num1 ** num2 = " + exp_res)
+print("num1 + num2 = " + str(add_res))
+print("num1 - num2 = " + str(sub_res))
+print("num1 * num2 = " + str(mul_res))
+print("num1 / num2 = " + str(div_res))
+print("num1 // num2 = " + str(floor_res))
+print("num1 % num2 = " + str(mod_res))
+print("num1 ** num2 = " + str(exp_res))
