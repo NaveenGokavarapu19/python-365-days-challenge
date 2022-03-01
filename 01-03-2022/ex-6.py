afeb8445970997202035877d63333053f5f9b315
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 15:26:42 +0530

    program to read a character in uppercase and then print in lower case

---
+++ 01-03-2022/ex-6.py
@@ -0,0 +1,6 @@
+'''
+program to read a character in uppercase and then print in lower case
+'''
+
+letter_to_de_displayed = input("Enter the letter to be displayed in lower case: ").upper()
+print("The letter " + letter_to_de_displayed + " is " + letter_to_de_displayed.lower())
