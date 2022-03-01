4ed5793c14678acb7afc8c503b32a8cd5515ecc4
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 16:06:50 +0530

    program to take input of two floating numbers, add and display result as a integer.

---
+++ 01-03-2022/ex11.py
@@ -0,0 +1,9 @@
+'''
+program to take input of two floating numbers, add and display result as a integer
+'''
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+c = int(a+b)
+
+print("The values of a,b,c are: ", a, b ,c)
