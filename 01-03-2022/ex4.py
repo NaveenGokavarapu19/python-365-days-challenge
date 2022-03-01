ca3d6b5e2ef2db292d98dd943deaf6fc8cf12847
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 14:00:40 +0530

    program to demonstrate usage of strings with single qoutes, double qoutes and triple qoutes

---
+++ 01-03-2022/ex4.py
@@ -0,0 +1,11 @@
+'''
+program to demonstrate usage of strings with single qoutes, double qoutes and triple qoutes
+'''
+
+single_qoute = 'Today is boring'
+double_qoute = "Might be tommorow might be exciting"
+triple_qoute = "A day is exicting or boring is completely dependent on the person rather than the day"
+
+print(single_qoute)
+print(double_qoute)
+print(triple_qoute)
