3faf6f8806fe88dd9043c0c499082cdaeec072dd
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 15:28:35 +0530

    Program to print ascii value of a character

---
+++ 01-03-2022/ex-5.py
@@ -0,0 +1,8 @@
+'''
+Program to print ascii value of a character
+'''
+
+letter = input("Enter the letter for which ascii value is to be displayed: ")
+ascii_value = ord(letter)
+
+print("The ascii value for " + letter + " is " + str(ascii_value))
