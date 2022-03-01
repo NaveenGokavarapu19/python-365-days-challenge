a6d5b2140dbf404f32602bea2e04044f39b09eb9
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-01 15:56:43 +0530

    program to demonstrate implict and explict conversion

---
+++ 01-03-2022/ex-8and9.py
@@ -0,0 +1,25 @@
+'''
+program to demonstrate implict and explict conversion.
+
+'''
+
+# Type casting is a method to convert the variable data into certain type in order perform the operations required by the user.
+
+
+# In implict conversion python automatically converts one data type into another automatically
+# eg a to int
+
+a = 7
+print(type(a))
+
+# In explict conversion  
+# In explict conversion python requires user intervention to convert one data type to another
+#type casting
+
+# int variable
+
+a = 5
+
+n = float(a)
+
+print(type(n))
