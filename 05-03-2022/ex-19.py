df388c80bbc4da7560369ea0af1223d762380838
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-05 15:00:10 +0530

    Program to display message in particular format

---
+++ 05-03-2022/ex-19.py
@@ -0,0 +1,11 @@
+'''
+Program to display message in following way
+
+Hello firstname lastname
+Welcome to Python!
+'''
+first_name = input("Enter the first name to be displayed: ")
+last_name = input("Enter the last name: ")
+
+print("Hello " + first_name + " " + last_name )
+print("Welcome to Python: ")
