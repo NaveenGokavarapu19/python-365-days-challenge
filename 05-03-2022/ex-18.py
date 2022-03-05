7cc0dd443ec3f2c0861327dbd75a26125853019e
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-05 14:56:20 +0530

    Program to Calculate number of seconds in a day

---
+++ 05-03-2022/ex-18.py
@@ -0,0 +1,10 @@
+'''
+Program to Calculate number of seconds in a day
+
+'''
+
+print("This program is about displaying number of seconds in a day: ")
+
+number_of_seconds_day = 24 * 3600
+
+print("The number of seconds in a day is: " + str(number_of_seconds_day))
