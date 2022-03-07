526af2924160d0896acec4573f0bbf9b90d88c40
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-07 17:46:47 +0530

    program to display day corresponding to week

---
+++ chapter-4/07-03-2022/program-11.py
@@ -0,0 +1,22 @@
+'''
+program to display day corresponding to week
+
+'''
+
+num = int(input("Enter any number between 1 to 7: "))
+if(num==1):
+    print("Sunday")
+elif(num==2):
+    print("Monday")
+elif(num==3):
+    print("Tuesday")
+elif(num==4):
+    print("Wednesday")
+elif(num==5):
+    print("Thrusday")
+elif(num==6):
+    print("Friday")
+elif(num==7):
+    print("Saturday")
+else:
+    print("Wrong input")
