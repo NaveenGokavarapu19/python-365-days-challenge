dc93936d6134febeaad0d0fe33259999571b6a90
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-07 12:04:00 +0530

    Program to find whether the given number is even or odd

---
+++ chapter-4/07-03-2022/program-5.py
@@ -0,0 +1,10 @@
+'''
+Program to find whether the given number is even or odd.
+
+'''
+
+num = int(input("Enter any number: "))
+if(num%2==0):
+    print(num,"is even")
+else:
+    print(num,"is odd")
