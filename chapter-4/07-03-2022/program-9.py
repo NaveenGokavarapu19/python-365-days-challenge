1a0912c4f32babbdfc2bceb4674ddb005ec456f2
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-07 17:31:28 +0530

    Program to determine whether the character entered is a vowel or not

---
+++ chapter-4/07-03-2022/program-9.py
@@ -0,0 +1,11 @@
+'''
+Program to determine whether the character entered is a vowel or not.
+
+'''
+ch = input("Enter any character: ")
+if (ch == "A" or ch=='E' or ch =='I' or ch == "O" or ch == "U"):
+    print(ch, "is a vowel")
+elif(ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u"):
+    print(ch, "is a vowel")
+else:
+    print(ch, "is not vowel")
