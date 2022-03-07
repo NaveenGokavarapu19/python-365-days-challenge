0637ee78d25183bda5625015eac063d5aac3e864
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-07 11:56:04 +0530

    program to find the largest of two numbers

---
+++ chapter-4/07-03-2022/program-4.py
@@ -0,0 +1,13 @@
+'''
+program to find the largest of two numbers.
+
+'''
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+if (a>b):
+    large = a
+else:
+    large = b 
+
+print("Large = ",large)
