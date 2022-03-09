ebacfda0aecbc566212dd0ca9b8ba2bc3e0d2f92
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-09 20:57:08 +0530

    program to calculate GCD of two numbers.

---
+++ chapter-4/09-03-2022/problem-26.py
@@ -0,0 +1,20 @@
+'''
+program to calculate GCD of two numbers.
+
+'''
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if(num1>num2):
+    dividend = num1
+    divisor = num2
+else:
+    dividend = num2
+    divisor = num1
+
+while(divisor!=0):
+    remainder = dividend / divisor
+    divisor = remainder
+
+print("GCD of ",num1," and ",num2," is ",dividend)
