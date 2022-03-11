8eb4a0007b61c4060e3d9f69c7787810bef36097
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-11 11:09:07 +0530

    program to calculate the factorial of a number

---
+++ chapter-4/11-03-2022/problem-32.py
@@ -0,0 +1,13 @@
+'''
+program to calculate the factorial of a number.
+
+'''
+
+num = int(input("Enter the number: "))
+if(num==0):
+    fact=1
+fact=1
+
+for i in range(1,num+1):
+    fact = fact*i
+print("Factorial of",num,"is",fact)
