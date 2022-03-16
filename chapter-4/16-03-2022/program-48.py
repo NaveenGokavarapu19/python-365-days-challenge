4e647e3b7047c62d84bf674ddeb926097752c842
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-16 12:05:00 +0530

    program to print the following pattern mentioned in docstrings

---
+++ chapter-4/16-03-2022/program-48.py
@@ -0,0 +1,17 @@
+'''
+program to print the following pattern mentioned in docstrings.
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+'''
+
+for i in range(1,6):
+    print()
+    for j in range(1,i+1):
+        print(j,end = " ")
+
+print("\n")
