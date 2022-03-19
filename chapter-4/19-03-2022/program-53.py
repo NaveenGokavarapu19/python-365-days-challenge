1fedfef65746985c4f110e79bf3ae72d828748bb
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-19 23:32:51 +0530

    program to print the pattern mentioned in the doc strings.  1  2 2  3 3 3  4 4 4 4  5 5 5 5 5 .

---
+++ chapter-4/19-03-2022/program-53.py
@@ -0,0 +1,18 @@
+'''
+program to print the pattern mentioned in the doc strings.
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+
+'''
+
+N = 5
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        print(i,end= " ")
+    print()
+print("\n")
