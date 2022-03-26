e2c3f6281d801c891c2b052be5975aaee68aa1be
Naveen <gokavarapu.naveen97@gmail.com>
2022-03-26 22:19:05 +0530

    program to display first 10 numbers.

---
+++ chapter-4/26-05-2022/ex-8.py
@@ -0,0 +1,11 @@
+'''
+program to display first 10 numbers.
+
+'''
+
+sum_of_numbers=0
+
+for i in range(11):
+    sum_of_numbers = sum_of_numbers + i
+
+print("The sum of the first 10 numbers is: " + str(sum_of_numbers))
